440ad07d45ce925557f93c2926716506fce546e1
vbnm95 <vbnm95@naver.com>
2023-07-31 14:49:30 +0900

    first commit

---
+++ src/util/Alpha.js
@@ -0,0 +1,44 @@
+import VT100 from "./VT100.js";
+
+class Alpha {
+	line = 0;
+	column = 0;
+	fg = 0;
+	bg = 0;
+	ch = "";
+	
+	constructor() {
+		this.line = parseInt(Math.random()*20+1);	
+		this.column = parseInt(Math.random()*40+1);	
+		
+		do {
+		this.fg = parseInt(Math.random()*8+30);
+		this.bg = parseInt(Math.random()*8+40);
+		} while(this.fg+10 == this.bg);
+		
+		this.ch = String.fromCharCode(parseInt(Math.random()*26+"A"[0].charCodeAt()));
+	}
+	
+	show() {
+		VT100.cursorMove(this.line, this.column);
+		VT100.setForeground(this.fg);
+		VT100.setBackground(this.bg);
+		VT100.print(this.ch);
+	}
+	
+	hide() {
+		VT100.cursorMove(this.line, this.column);
+		VT100.reset();
+		VT100.print(" ");
+	}
+	
+	getLine() {
+		return this.line;
+	}
+	
+	getColumn() {
+		return this.column;
+	}
+}
+
+export default Alpha;
